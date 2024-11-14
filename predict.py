0d02bf2df2232823489c99c82b329f0a78bbbd11
bdehapiot <b.dehapiot@gmail.com>
2024-11-14 16:35:40 +0100

    update

--- predict.py
+++ predict.py
@@ -1,29 +1,52 @@
 #%% Imports -------------------------------------------------------------------
 
+import numpy as np
 from skimage import io
 from pathlib import Path
+
+# Functions 
+from functions import preprocess_image
 
 # bdmodel
 from bdmodel.predict import predict
 
+# skimage
+from skimage.trasform import rescale
+
 #%% Inputs --------------------------------------------------------------------
 
 # Paths
-img_path = Path(Path.cwd(), "data", "240611-12_2 merged_pix(13.771)_00.tif")
+path = Path(Path.cwd(), "data", "240611-12_2 merged_pix(13.771)_00.tif")
 model_path = Path(Path.cwd(), "model_mass")
+
+# Pixel size
+pixSize_key = 3.731 # reference
+pixSize_o10 = 2.347
+pixSize_o06 = 3.676
 
 #%% Execute -------------------------------------------------------------------
 
 if __name__ == "__main__":
     
-    # Open data
-    img = io.imread(img_path)
+    # Open & preprocess image
+    img = io.imread(path)
+    img = np.mean(img, axis=2) # RGB to float
+    img = preprocess_image(img)
+    
+    # Rescale images
+    if "keyence" in str(path.resolve()):
+        pass
+    if "ozp" in str(path.resolve()):
+        if "mag10" in str(path.resolve()):
+            img = rescale(img, pixSize_o10 / pixSize_key)
+        if "mag06" in str(path.resolve()):
+            img = rescale(img, pixSize_o06 / pixSize_key)
     
     # Predict
     prds = predict(        
         img,
         model_path,
-        img_norm="global",
+        img_norm="image",
         patch_overlap=0,
         )
     
