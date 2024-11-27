333030ba2ddb45a4f64ad28e1100de14e3d92a6a
bdehapiot <b.dehapiot@gmail.com>
2024-11-27 13:46:27 +0100

    update

--- main_samples.py
+++ main_samples.py
@@ -1,6 +1,5 @@
 #%% Imports -------------------------------------------------------------------
 
-import numpy as np
 from skimage import io
 from pathlib import Path
 
@@ -10,12 +9,11 @@ from functions import process
 #%% Inputs --------------------------------------------------------------------
 
 # Paths
-data_path = Path("D:\local_CapsSeg\data")
-smpl_path = Path("D:\local_CapsSeg\samples")
+smpl_path = Path("D:\local_CapsSeg\samples\keyence")
 img_paths = (
-    list(data_path.glob("**/*.jpg")) + 
-    list(data_path.glob("**/*.png")) +
-    list(data_path.glob("**/*.tif"))
+    list(smpl_path.glob("**/*.jpg")) + 
+    list(smpl_path.glob("**/*.png")) +
+    list(smpl_path.glob("**/*.tif"))
     )
 model_cores_path = Path(Path.cwd(), "model_cores_edt_512_gamma")
 model_shell_path = Path(Path.cwd(), "model_shell_edt_512_gamma")
@@ -29,16 +27,8 @@ overlap = 256
 
 if __name__ == "__main__":
     
-    np.random.seed(42)
-    idxs = np.random.choice(
-        range(len(img_paths) + 1),
-        size=nImg, replace=False
-        )
-    
-    for idx in idxs:
-        
-        path = img_paths[idx]
-                
+    for path in img_paths:
+                        
         outputs = process(
             path, overlap,
             model_cores_path,
@@ -46,8 +36,6 @@ if __name__ == "__main__":
             rf=rf, save=False
             )
         
-        img_path = Path(smpl_path, (path.stem + ".tif"))
-        io.imsave(img_path, outputs["img"])
-        display_path = Path(smpl_path, (path.stem + "_display_01.png"))
+        display_path = Path(smpl_path.parent, (path.stem + "_display_01.png"))
         io.imsave(display_path, outputs["rgbDisplay"])
     
